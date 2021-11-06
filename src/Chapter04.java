74fba2ccdd836721b8e03b2efb6c20318b75ae30
chacolate33 <ko7ta7ko7ta@gmail.com>
2021-11-06 19:54:24 +0900

    finish 4

---
+++ src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	
+	public static void main (String[] args) {
+		String name = "kotaro";
+		System.out.println(name);
+		name = "lukey";
+		int num1 = 7;
+		System.out.println(num1);
+		
+	}
+	
+}
