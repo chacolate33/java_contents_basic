bba58e7a9dad38dbc55484aec46c4b0bba01fffd
chacolate33 <ko7ta7ko7ta@gmail.com>
2021-11-09 17:32:02 +0900

    finish 6

---
+++ src/Chapter06.java
@@ -0,0 +1,21 @@
+
+public class Chapter06 {
+	public static void main(String[] args) {
+//		String a = "applePie";
+//		String f = new String("applePie");
+//		boolean c;
+//		c = a.equals(f);
+//		System.out.println(c);
+		
+		int val = 2 + 5;
+		System.out.println(val);
+		val++;
+		System.out.println(val);
+		val *= 5;
+		System.out.println(val);
+		boolean bool = val == 50;
+		System.out.println(bool);
+		bool = val < 50 && val % 10 == 0;
+		System.out.println(bool);
+	}
+}
