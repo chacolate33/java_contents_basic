8c578009f8b93c552b0f1547483f6185bea46e59
chacolate33 <ko7ta7ko7ta@gmail.com>
2021-11-11 18:38:15 +0900

    finish 8

---
+++ src/Chapter08.java
@@ -0,0 +1,25 @@
+
+public class Chapter08 {
+	public static void main(String[] args) {
+		int number = 1;
+		while (number < 5) {
+			int answer;
+			answer =  number * number;
+			System.out.println(answer);
+			number ++;
+		}
+		
+		int[] array = {3, 10, 9, 2};
+		for (int count = 0; count < array.length; count++) {
+			System.out.println(array[count]);
+		}
+		
+		for (int content :array) {
+			if (content % 2 == 0) {
+				continue;				
+			}
+			System.out.println(content);
+			
+		}
+	}
+}
