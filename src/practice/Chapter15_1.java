be2c89027fbc561b295445be166c418bc585a92d
chacolate33 <ko7ta7ko7ta@gmail.com>
2021-12-01 11:41:38 +0900

    finish 15 file

--- src/practice/Chapter15_1.java
+++ src/practice/Chapter15_1.java
@@ -5,7 +5,6 @@ public class Chapter15_1 {
 		int a = 976;
 		int b = 427;
 		System.out.println(a + "と" + b + "の最大公約数は、" + gcd(a, b) + "です。");
-		System.out.println(148 % 28);
 	}
 	
 	static int gcd(int a, int b) {
