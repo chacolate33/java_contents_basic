243353dff33bc3217edfc3be33603f6de5c8a07e
chacolate33 <ko7ta7ko7ta@gmail.com>
2021-12-18 20:55:10 +0900

    finish lesson08_05

---
+++ KinoCode/Lesson06_01.java
@@ -0,0 +1,21 @@
+
+public class Lesson06_01 {
+	public static void main(String[] args) {
+
+//		int var-01 = 5;
+//		int 01var = 6;
+//		
+//		
+//		System.out.println(var-01);
+//		System.out.println(01var);
+		
+//		int VAR = 1;
+//		int Var = 2;
+//		
+//		System.out.println(VAR);
+//		System.out.println(Var);
+		
+//		int return = 1;
+//		int class = 1;
+	}
+}
