f639fec3c603d5a2765b8668253f6939641fa547
chacolate33 <ko7ta7ko7ta@gmail.com>
2021-12-28 21:20:48 +0900

    edit 24

---
+++ HelloWorld/StaticSample.java
@@ -0,0 +1,9 @@
+
+public class StaticSample {
+	int num = 0;
+	static int staticNum = 0;
+	public StaticSample() {
+		num++;
+		staticNum++;
+	}
+}
---
+++ HelloWorld/UseStaticSample.java
@@ -0,0 +1,16 @@
+
+public class UseStaticSample {
+	public static void main(String[] args) {
+		System.out.println("クラス変数 staticNum = " + StaticSample.staticNum);
+		System.out.println();
+		StaticSample ss1 = new StaticSample();
+		System.out.println("-- ss1オブジェクト --");
+		System.out.println("インスタンス変数 num = " + ss1.num);
+		System.out.println("クラス変数 staticNum = " + ss1.staticNum);
+		System.out.println();
+		StaticSample ss2 = new StaticSample();
+		System.out.println("-- ss2オブジェクト");
+		System.out.println("インスタンス変数 num = " + ss2.num);
+		System.out.println("クラス変数 staticNum = " + ss2.staticNum);
+	}
+}
